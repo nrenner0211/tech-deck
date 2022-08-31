c89b66795c68c5fffb6f203ca555a5d104f8d848
nrenner <nrenner0211@gmail.com>
2022-08-31 18:39:57 -0400

    routes functional again

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -54,7 +54,6 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -76,7 +75,6 @@ router.post("/", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email,
@@ -115,9 +113,6 @@ router.post("/logout", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
-  // pass in req.body instead to only update what's passed through
   User.update(req.body, {
     individualHooks: true,
     where: {
--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,7 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_url = document.querySelector('input[name="post-url"]').value;
+  const post_url = document.querySelector('textarea[name="post-url"]').value.trim();
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
